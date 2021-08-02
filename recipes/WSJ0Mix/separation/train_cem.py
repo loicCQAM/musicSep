f86bd3a990df67c8f9b60a50ed34fa55ef993036
loiCQAM <loicprenevost@gmail.com>
2021-08-02 08:26:47 -0400

    remove prints

--- recipes/WSJ0Mix/separation/train_cem.py
+++ recipes/WSJ0Mix/separation/train_cem.py
@@ -85,8 +85,8 @@ class Separation(sb.Brain):
     def compute_objectives(self, predictions, targets):
         """Computes the sinr loss"""
         #return self.hparams.loss(predictions=predictions, targets=targets)
-        print(predictions.shape)
-        print(targets.shape)
+        #print(predictions.shape)
+        #print(targets.shape)
         return self.hparams.loss(source=targets, estimate_source=predictions)
 
     def fit_batch(self, batch):
