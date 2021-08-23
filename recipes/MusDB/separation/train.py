8d7b5d5220ca690345e9aa22fc5431f8bc4b6c08
loiCQAM <loicprenevost@gmail.com>
2021-08-23 13:48:28 -0400

    ...

--- recipes/MusDB/separation/train.py
+++ recipes/MusDB/separation/train.py
@@ -422,7 +422,7 @@ if __name__ == "__main__":
     if not hparams["test_only"]:
         train_set = Rawset(
             os.path.join(hparams["musdb_raw_path"], "train"),
-            samples=hparams["sample_rate"] * 5,
+            samples=hparams["sample_rate"] * hparams["sequence_length"],
             channels=2,
             streams=[0, 1, 2, 3, 4],
             stride=hparams["sample_rate"],
@@ -434,7 +434,7 @@ if __name__ == "__main__":
 
         valid_set = Rawset(
             os.path.join(hparams["musdb_raw_path"], "valid"),
-            samples=hparams["sample_rate"] * 5,
+            samples=hparams["sample_rate"] * hparams["sequence_length"],
             channels=2,
             streams=[0, 1, 2, 3, 4],
             stride=hparams["sample_rate"],
