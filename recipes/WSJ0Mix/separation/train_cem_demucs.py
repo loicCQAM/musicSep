b81183808b03b290e52afd0fe23cd4d5c4eb144b
loiCQAM <loicprenevost@gmail.com>
2021-08-24 08:22:53 -0400

    shorter

--- recipes/WSJ0Mix/separation/train_cem_demucs.py
+++ recipes/WSJ0Mix/separation/train_cem_demucs.py
@@ -503,7 +503,7 @@ class musdb_test_dataset(Dataset):
 
     def __getitem__(self, idx):
         track = self.mus.tracks[idx]
-        track.chunk_duration = 10
+        track.chunk_duration = 5
         track.chunk_start = np.random.uniform(
             0, track.duration - track.chunk_duration)
         x = torch.from_numpy(track.audio.T).float()
